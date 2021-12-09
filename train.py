a652e182f8e9e39ea4f475ea4d1ddf4e15391c6a
shashankag14 <shashankag14@gmail.com>
2021-12-09 16:32:40 +0100

    Train/Val loop with stable training. Bleu always zero

--- train.py
+++ train.py
@@ -1,8 +1,195 @@
-import torch
-from model.transformer import Transformer
+"""
+@author : Shashank Agarwal
+@when : 06-12-2021
+@homepage : https://github.com/shashankag14
+"""
 
-src = torch.rand(64, 16, 512)
-tgt = torch.rand(64, 16, 512)
-out = Transformer()(src, tgt)
-print(out.shape)
-# torch.Size([64, 16, 512])
+import torch
+import math
+import time
+
+from torch import nn, optim
+from torch.optim import Adam
+from torchtext.data.metrics import bleu_score
+import nltk
+
+# Local project files
+import utils
+from model.transformer import Transformer
+from utils import *
+import dictionary as dict
+import tokenizer
+import dataloader
+from bleu_metric import *
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+MODEL_SANITY_CHECK = 0
+
+if MODEL_SANITY_CHECK:
+	src = torch.rand(25, 16)  # batch_size, seq_length
+	tgt = torch.rand(25, 16)  # batch_size, seq_length
+	out = Transformer(207, 183, 0, 0, "cpu")(src, tgt)
+	print("MODEL OUTPUT SHAPE : ", out.shape)# torch.Size([batch_size, max_sent_len, tgt_vocab_size])
+
+# Count the number of trainable parameters in the model 
+def count_parameters(model):
+	return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def initialize_weights(m):
+	if hasattr(m, 'weight') and m.weight.dim() > 1:
+		nn.init.kaiming_uniform(m.weight.data)
+
+# Create dictionary
+corpus = tokenizer.Corpus()
+src_vocab_size = corpus.dictionary_src.n_word
+tgt_vocab_size = corpus.dictionary_tgt.n_word
+print("SRC_VOCAB_SIZE {}, TGT_VOCAB_SIZE {}".format(src_vocab_size, tgt_vocab_size))
+
+# Create datasets
+train_dataloader, valid_dataloader, test_dataloader = dataloader.get_dataloader(corpus.tokenize_src,
+                                                                                corpus.tokenize_tgt)
+# Create model instance
+model = Transformer(src_vocab_size,
+                    tgt_vocab_size,
+                    src_mask_idx=dict.PAD_token,
+                    tgt_mask_idx=dict.PAD_token,
+                    device=device,
+                    num_encoder_layers=utils.n_layers,
+                    num_decoder_layers=utils.n_layers,
+                    dim_model=utils.d_model,
+                    num_heads=utils.n_heads,
+                    dim_feedforward=utils.ffn_hidden,
+                    dropout=utils.dropout, ).to(device)
+
+print(f'Trainable Parameters : {count_parameters(model):,}')
+model.apply(initialize_weights)
+
+# Optimizer
+optimizer = Adam(params=model.parameters(),
+                 lr=init_lr,
+                 weight_decay=weight_decay,
+                 eps=adam_eps)
+
+# LR Scheduler for better gradient descent 
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,
+                                                 verbose=True,
+                                                 factor=factor,
+                                                 patience=patience)
+
+# Loss function (Cross entropy)
+criterion = nn.CrossEntropyLoss(ignore_index=dict.PAD_token)
+
+# Train loop for each epoch
+def train(model, iterator, optimizer, criterion, clip):
+	model.train()
+	epoch_loss = 0
+	for i, batch in enumerate(iterator):
+		src = batch[0].to(device)
+		trg = batch[1].to(device)
+
+		optimizer.zero_grad()
+		output = model(src, trg)  # trg[:,:-1] doesnt work as output is [N,seq_len] and so is src
+
+		# Reshape output and trg before computing the loss
+		output_reshape = output[:, 1:].contiguous().view(-1, output.shape[-1])
+		trg = trg[:, 1:].contiguous().view(-1) # removing the first token of <SOS> and then flattening 2D to 1D tensor
+
+		# output_reshape : (N*seq_len, model_dim), tgt : (N*seq_len)
+		# Compute batch loss
+		loss = criterion(output_reshape, trg)
+
+		# Backprop
+		loss.backward()
+
+		# Clip gradients to avoid exploding gradient issues
+		#torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+		optimizer.step()
+
+		epoch_loss += loss.item()
+		print('step :', round((i / len(iterator)) * 100, 2), '% , loss :', loss.item())
+
+	return epoch_loss / len(iterator)
+
+# Validation loop for each epoch
+def evaluate(model, iterator, criterion):
+	model.eval()
+	epoch_loss = 0
+	batch_bleu = []
+	with torch.no_grad():
+		for i, batch in enumerate(iterator):
+			src = batch[0].to(device)
+			trg = batch[1].to(device)  
+
+			output = model(src, trg)
+			output_reshape = output[:,1:].contiguous().view(-1, output.shape[-1])
+			trg_reshape = trg[:, 1:].contiguous().view(-1)
+
+			loss = criterion(output_reshape, trg_reshape)
+			epoch_loss += loss.item()
+      
+      # Compute BLEU score per batch - corpus level or sentence level??
+			total_bleu = [] 
+			# Note : Size of last batch might not be equal to batch_size, thus trg.size(dim=0)
+			for j in range(trg.size(dim=0)):
+				trg_words = idx_to_word(trg[j], corpus.dictionary_tgt)
+				output_words = output[j].max(dim=1)[1]
+				output_words = idx_to_word(output_words, corpus.dictionary_tgt)
+				# print(trg_words, output_words)        
+				#print("trg", trg_words.split(), "out", output_words.split())
+				bleu = get_bleu(hypotheses=output_words.split(), reference=trg_words.split())
+				# bleu = nltk.translate.bleu_score.sentence_bleu(output_words.split(), trg_words.split())
+				bleu = bleu*100
+				total_bleu.append(bleu)
+				print(bleu)      
+
+			total_bleu = sum(total_bleu) / len(total_bleu)
+			batch_bleu.append(total_bleu)
+
+	batch_bleu = sum(batch_bleu) / len(batch_bleu)
+	return epoch_loss / len(iterator), batch_bleu
+
+
+def run(total_epoch, best_loss):
+	train_losses, test_losses, bleus = [], [], []
+	for step in range(total_epoch):
+		start_time = time.time()
+		train_loss = train(model, train_dataloader, optimizer, criterion, clip)
+		valid_loss, bleu = evaluate(model, valid_dataloader, criterion)
+		end_time = time.time()
+
+		if step > warmup:
+			scheduler.step(valid_loss)
+
+		train_losses.append(train_loss)
+		test_losses.append(valid_loss)
+		bleus.append(bleu)
+		epoch_mins, epoch_secs = compute_time(start_time, end_time)
+
+		if valid_loss < best_loss:
+			best_loss = valid_loss
+			torch.save(model.state_dict(), 'saved_chkpt/model-{0}.pt'.format(valid_loss))
+
+		f = open('results/train_loss.txt', 'w')
+		f.write(str(train_losses))
+		f.close()
+
+		f = open('results/bleu.txt', 'w')
+		f.write(str(bleus))
+		f.close()
+
+		f = open('results/test_loss.txt', 'w')
+		f.write(str(test_losses))
+		f.close()
+
+		print(f'Epoch: {step + 1} | Time: {epoch_mins}m {epoch_secs}s')
+		print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+		print(f'\tVal Loss: {valid_loss:.3f} |  Val PPL: {math.exp(valid_loss):7.3f}')
+		print(f'\tBLEU Score: {bleu:.3f}')
+
+
+if __name__ == '__main__':
+	run(total_epoch=epoch, best_loss=inf)
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@
 """
 
 import torch
+import os
 
 # GPU device setting
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -15,6 +16,14 @@ tgt_ext = "en"
 src_data_path = "data/PHP.cs-en." + src_ext
 tgt_data_path = "data/PHP.cs-en." + tgt_ext
 
+saved_chkpt = 'saved_chkpt/'
+if not os.path.exists(saved_chkpt):
+  os.mkdir(saved_chkpt) 
+
+results = 'results/'
+if not os.path.exists(results):
+  os.mkdir(results) 
+
 # model parameter setting
 batch_size = 128
 max_len = 256 # Where is it being used??
@@ -22,12 +31,12 @@ d_model = 512
 n_layers = 6
 n_heads = 8
 ffn_hidden = 2048
-drop_prob = 0.1
+dropout = 0.4
 
 max_sent_len = 14
 
 # optimizer parameter setting
-init_lr = 1e-5
+init_lr = 1e-3
 factor = 0.9
 adam_eps = 5e-9
 patience = 10
