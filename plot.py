3b1f86e981b3e7913def93029028d1dd2cd904b9
shashankag14 <shashankag14@gmail.com>
2021-12-12 01:07:23 +0100

    Removed checkpoint due to large size

---
+++ plot.py
@@ -0,0 +1,38 @@
+"""
+@author : Shashank Agarwal
+@when : 11-12-2021
+@homepage : https://github.com/shashankag14
+"""
+
+import matplotlib.pyplot as plt
+from utils import *
+
+def convert_to_list(file):
+	with open(file, 'r') as f:
+		file_str = f.read()
+		file_str = file_str.strip('[]').split(',')
+		file_list = [float(val) for val in file_str]
+	f.close()
+	return file_list
+
+def create_plots():
+	train_loss = convert_to_list(train_loss_file)
+	valid_loss = convert_to_list(valid_loss_file)
+	bleu_loss = convert_to_list(bleu_loss_file)
+	num_epochs = len(train_loss)
+
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+
+	ax1.set_title("Training/Validation Loss vs Epoch")
+	ax1.set_xlabel('Epochs')
+	ax1.set_ylabel('Loss')
+
+	ax1.plot(range(1,num_epochs+1), train_loss, c='r', label='Training Loss')
+	ax1.plot(range(1, num_epochs + 1), valid_loss, c='b', label='Validation Loss')
+	leg = ax1.legend()
+	plt.grid()
+	plt.show()
+
+if __name__ == '__main__':
+	create_plots()
---
+++ test.py
@@ -0,0 +1,71 @@
+"""
+@author : Shashank Agarwal
+@when : 10-12-2021
+@homepage : https://github.com/shashankag14
+"""
+
+from model.transformer import Transformer
+from utils import *
+import dictionary as dict
+import tokenizer
+import dataloader
+from bleu_metric import *
+
+print("Test phase started.")
+
+# Create dictionary
+corpus = tokenizer.Corpus()
+src_vocab_size = corpus.dictionary_src.n_word
+tgt_vocab_size = corpus.dictionary_tgt.n_word
+
+# Create model instance
+model = Transformer(src_vocab_size,
+                    tgt_vocab_size,
+                    src_mask_idx=dict.PAD_token,
+                    tgt_mask_idx=dict.PAD_token,
+                    device=device,
+                    num_encoder_layers=n_layers,
+                    num_decoder_layers=n_layers,
+                    dim_model=d_model,
+                    num_heads=n_heads,
+                    dim_feedforward=ffn_hidden,
+                    dropout=dropout).to(device)
+
+# Create datasets
+_, _, test_dataloader = dataloader.get_dataloader(corpus.tokenize_src,corpus.tokenize_tgt)
+print("Size of Test datasets :", len(test_dataloader))
+
+def test_model(dataloader):
+    model.load_state_dict(torch.load("saved_chkpt/best_model.pt"))
+    with torch.no_grad():
+        batch_bleu = []
+        for i, batch in enumerate(dataloader):
+            src = batch[0].to(device)
+            trg = batch[1].to(device)
+            output = model(src, trg)
+
+            total_bleu = []
+            for j in range(trg.size(dim=0)):
+                src_words = idx_to_word(src[j], corpus.dictionary_src)
+                trg_words = idx_to_word(trg[j], corpus.dictionary_tgt)
+                output_words = output[j].max(dim=1)[1]
+                output_words = idx_to_word(output_words, corpus.dictionary_tgt)
+
+                print('source :', src_words)
+                print('target :', trg_words)
+                print('predicted :', output_words)
+                print()
+
+                bleu = get_bleu(hypotheses=output_words.split(), reference=trg_words.split())
+                total_bleu.append(bleu)
+
+            total_bleu = sum(total_bleu) / len(total_bleu)
+            print('BLEU SCORE = {}'.format(total_bleu))
+            batch_bleu.append(total_bleu)
+
+        batch_bleu = sum(batch_bleu) / len(batch_bleu)
+        print('TOTAL BLEU SCORE = {}'.format(batch_bleu))
+
+
+if __name__ == '__main__':
+    test_model(test_dataloader)
