10ebac6c458b98fd40e8e43e2e79f5d82e3933c9
shashankag14 <shashankag14@gmail.com>
2021-12-14 18:57:42 +0100

    Added argument parser in utils

--- utils.py
+++ utils.py
@@ -1,16 +1,18 @@
-"""
-@author : Shashank Agarwal
-@when : 06-12-2021
-@homepage : https://github.com/gusdnd852
-"""
-
-import torch
+import argparse
 import os
+import torch
+
+# For monitoring epoch time
+def compute_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
 
 # GPU device setting
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-## arguement parser to be added
+# Directories for local files
 src_ext = "cs"
 tgt_ext = "en"
 src_data_path = "data/PHP.cs-en." + src_ext
@@ -18,37 +20,75 @@ tgt_data_path = "data/PHP.cs-en." + tgt_ext
 
 saved_chkpt = 'saved_chkpt/'
 if not os.path.exists(saved_chkpt):
-  os.mkdir(saved_chkpt) 
+  os.mkdir(saved_chkpt)
 
 results = 'results/'
 if not os.path.exists(results):
-  os.mkdir(results) 
+  os.mkdir(results)
+
+# Argument parser
+# For data files
+parser = argparse.ArgumentParser(description='train')
+parser.add_argument('--src_data', type=str, default=src_data_path,
+                    help='location of the src data')
+parser.add_argument('--tgt_data', type=str, default=tgt_data_path,
+                    help='location of the tgt data')
 
 # model parameter setting
-batch_size = 128
-max_len = 256 # Where is it being used??
-d_model = 512
-n_layers = 6
-n_heads = 8
-ffn_hidden = 2048
-dropout = 0.4
-
-max_sent_len = 16
+parser.add_argument('--batch_size', type=int, default=128, metavar='N',
+                    help='batch size')
+parser.add_argument('--d_model', type=int, default=512,
+                    help='size of word embeddings')
+parser.add_argument('--n_layers', type=int, default=6,
+                    help='number of enc/dec layers in each block')
+parser.add_argument('--n_heads', type=int, default=8,
+                    help='number of en/dec blocks')
+parser.add_argument('--ffn_hidden', type=int, default=2048,
+                    help='number of hidden units in FFN')
+parser.add_argument('--dropout', type=float, default=0.4,
+                    help='dropout probability')
+parser.add_argument('--max_sent_len', type=int, default=16,
+                    help='Maximum length of sentence to use for train/valid/test')
 
 # optimizer parameter setting
-init_lr = 5e-5
-factor = 0.9
-adam_eps = 5e-9
-patience = 8
-warmup = 100
-epoch = 1000
-clip = 1.0
-weight_decay = 5e-4
+parser.add_argument('--init_lr', type=float, default=5e-5,
+                    help='initial learning rate')
+parser.add_argument('--scheduler_factor', type=float, default=0.9,
+                    help='Factor with which LR will decreasing using scheduler (new_lr = old_lr * optim_factor)')
+parser.add_argument('--optim_adam_eps', type=float, default=5e-9,
+                    help='Adam epsilon')
+parser.add_argument('--optim_patience', type=int, default=8,
+                    help='Number of epochs optimizer will wait before decreasing LR')
+parser.add_argument('--optim_warmup', type=int, default=8,
+                    help='Optimizer warmup')
+parser.add_argument('--optim_weight_decay', type=int, default=100,
+                    help='Weight decay factor for optimizer')
+
+parser.add_argument('--epoch', type=int, default=1000,
+                    help='Number of epochs to train')
+parser.add_argument('--clip', type=float, default=1.0,
+                    help='Gradient clipping')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+
+args = parser.parse_args()
+
+# model parameter setting
+batch_size      = args.batch_size
+d_model         = args.d_model
+n_layers        = args.n_layers
+n_heads         = args.n_heads
+ffn_hidden      = args.ffn_hidden
+dropout         = args.dropout
+max_sent_len    = args.max_sent_len
+
+# optimizer parameter setting
+init_lr = args.init_lr
+factor = args.scheduler_factor
+adam_eps = args.optim_adam_eps
+patience = args.optim_patience
+warmup = args.optim_warmup
+epoch = args.epoch
+clip = args.clip
+weight_decay = args.optim_weight_decay
 inf = float('inf')
-
-
-def compute_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
